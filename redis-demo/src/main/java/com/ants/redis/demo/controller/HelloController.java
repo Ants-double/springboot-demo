ed6b1096398ddca0bdc1e5bc4c864e5338bf7a7e
liyy <ants_double@yeah.net>
2020-07-17 17:06:54 +0800

    redis

---
+++ redis-demo/src/main/java/com/ants/redis/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.ants.redis.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2020/7/17
+ */
+@RestController
+@RequestMapping(value = "/")
+public class HelloController {
+}
