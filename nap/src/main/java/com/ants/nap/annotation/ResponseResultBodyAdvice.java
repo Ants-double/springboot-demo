29335b05280e257322415f74c50b3f53a3e651a0
liyy <ants_double@yeah.net>
2021-07-01 15:17:07 +0800

    add base

---
+++ nap/src/main/java/com/ants/nap/annotation/ResponseResultBodyAdvice.java
@@ -0,0 +1,99 @@
+package com.ants.nap.annotation;
+
+import com.antsdouble.util.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import org.springframework.web.util.WebUtils;
+
+import javax.annotation.Resource;
+import java.lang.annotation.Annotation;
+
+/**
+ * @author lyy
+ * @Deprecated
+ * @date 2020/11/3
+ */
+@Slf4j
+@RestControllerAdvice
+public class ResponseResultBodyAdvice implements ResponseBodyAdvice<Object> {
+
+    private static final Class<? extends Annotation> ANNOTATION_TYPE = ResponseResultBody.class;
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> aClass) {
+        return AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), ANNOTATION_TYPE) || returnType.hasMethodAnnotation(ANNOTATION_TYPE);
+
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter methodParameter, MediaType mediaType, Class<? extends HttpMessageConverter<?>> aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+        if (body instanceof Result) {
+            return body;
+        }
+        if (body instanceof Resource) {
+            return body;
+        }
+        return Result.success(body);
+    }
+
+
+    /**
+     * 提供对标准Spring MVC异常的处理
+     *
+     * @param ex      the target exception
+     * @param request the current request
+     */
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Result<?>> exceptionHandler(Exception ex, WebRequest request) {
+        log.error("ExceptionHandler: {}", ex.getMessage());
+        HttpHeaders headers = new HttpHeaders();
+        if (ex instanceof Exception) {
+            return this.handleResultException((Exception) ex, headers, request);
+        }
+
+        return this.handleException(ex, headers, request);
+    }
+
+    /** 对ResultException类返回返回结果的处理 */
+    protected ResponseEntity<Result<?>> handleResultException(Exception ex, HttpHeaders headers, WebRequest request) {
+        Result<?> body = Result.error(ex.getMessage());
+        HttpStatus status =  HttpStatus.NON_AUTHORITATIVE_INFORMATION;
+        return this.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    /** 异常类的统一处理 */
+    protected ResponseEntity<Result<?>> handleException(Exception ex, HttpHeaders headers, WebRequest request) {
+        Result<?> body = Result.error(ex.getMessage());
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return this.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    /**
+     * org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler#handleExceptionInternal(java.lang.Exception, java.lang.Object, org.springframework.http.HttpHeaders, org.springframework.http.HttpStatus, org.springframework.web.context.request.WebRequest)
+     * <p>
+     * A single place to customize the response body of all exception types.
+     * <p>The default implementation sets the {@link WebUtils#ERROR_EXCEPTION_ATTRIBUTE}
+     * request attribute and creates a {@link ResponseEntity} from the given
+     * body, headers, and status.
+     */
+    protected ResponseEntity<Result<?>> handleExceptionInternal(
+            Exception ex, Result<?> body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        if (HttpStatus.INTERNAL_SERVER_ERROR.equals(status)) {
+            request.setAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE, ex, WebRequest.SCOPE_REQUEST);
+        }
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+}
---
+++ nap/src/test/java/com/ants/nap/NapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ants.nap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
