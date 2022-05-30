eb06aa17cc5cd1eba7519f9705b689009cfbefe7
Ants_double <ants_double@yeah.net>
2022-05-30 23:11:25 +0800

    configuration samples suspend

---
+++ configuration-cloud-samples/src/main/java/com/ants/configuration/cloud/samples/ConfigurationCloudSamplesApplication.java
@@ -0,0 +1,13 @@
+package com.ants.configuration.cloud.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationCloudSamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationCloudSamplesApplication.class, args);
+	}
+
+}
---
+++ configuration-cloud-samples/src/test/java/com/ants/configuration/cloud/samples/ConfigurationCloudSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ants.configuration.cloud.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationCloudSamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
