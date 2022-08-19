81814cb88276c050825114ff011d446de2fc41fb
forein <ants_double@qq.com>
2022-08-19 17:07:28 +0800

    sso

---
+++ sso-samples/src/main/java/com/ants/sso/samples/SsoSamplesApplication.java
@@ -0,0 +1,20 @@
+package com.ants.sso.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ants_
+ */
+@SpringBootApplication
+public class SsoSamplesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SsoSamplesApplication.class, args);
+
+
+
+    }
+
+}
---
+++ sso-samples/src/test/java/com/ants/sso/samples/SsoSamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ants.sso.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoSamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
